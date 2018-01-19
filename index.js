26e3fac7e2ccccff334673674f4553bbccd4df3d
stanciuionelamelania@gmail.com <stanciuionelamelania@gmail.com>
2018-01-19 09:38:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ function happyHolidaysTo(name){
 }
 
 function happyHolidayTo(holiday, name){
-  var x = 'Happy ${holiday}, ${name}!'
-  return x
+  return'Happy ${holiday}, ${name}!'
 }
 
 function holidayCountdown(holiday, days){
