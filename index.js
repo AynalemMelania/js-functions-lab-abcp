68c98496f39be2c61dbca87002f3a5a7090bdd21
stanciuionelamelania@gmail.com <stanciuionelamelania@gmail.com>
2018-01-19 09:32:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
-  return "Happy holidays," + name + "!"
+  return "Happy ${holiday}, ${name}!"
 }
 
 function happyHolidayTo(holiday, name){
